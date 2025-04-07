e8467a7ac20c3ff4377d905aa2b34c773bb3b349
rilagan06 <160271230+rilagan06@users.noreply.github.com>
2025-04-06 22:48:38 -0400

    added training model

---
+++ GJ_25/Assets/_MLAgent/Scripts/MoveToGoalsAgent.cs
@@ -0,0 +1,50 @@
+
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class MoveToGoals : Agent
+{
+    [SerializeField] private Transform targetTransform;
+    public override void OnEpisodeBegin()
+    {
+        transform.position = new Vector3(0, 0.5f, 0);
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.position);
+        sensor.AddObservation(targetTransform.position);
+    }
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+
+        float moveSpeed = 1f;
+        transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+        //Debug.Log(actions.DiscreteActions[0]);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Goal>(out Goal goal))
+        {
+            SetReward(+1f);
+            EndEpisode();
+        }
+        if (other.TryGetComponent<Wall>(out Wall wall))
+        {
+            SetReward(-1f);
+            EndEpisode();
+        }
+
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continousActions = actionsOut.ContinuousActions;
+        continousActions[0] = Input.GetAxisRaw("Horizontal");
+        continousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+}
